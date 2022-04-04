4c062314d98e8c26807eafcde2aa3850c8d2c078
rilopez99316 <ilbroberto99316@gmail.com>
2022-04-04 14:48:01 -0600

    generating data

--- DecisionTrees.py
+++ DecisionTrees.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def entropy(S):
   p1 = np.sum(S)/len(S)
@@ -15,3 +16,19 @@ S = [0,1,1,1,1]
 print(entropy(S))
 S = [0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
 print(entropy(S))
+
+def entropy_from_p1(p1):
+    p0 = 1-p1
+    if p0==0 or p1==0:
+        return 0
+    return -p0*np.log2(p0) - p1*np.log2(p1)
+
+p1 = np.linspace(0,1,100) # Generates 100 values from 0 to 1, unifirmly spaced
+
+ent = []
+for p in p1:
+  ent.append(entropy_from_p1(p))
+
+plt.plot(p1,ent)
+plt.xlabel('p(1)')
+plt.ylabel('entropy')
