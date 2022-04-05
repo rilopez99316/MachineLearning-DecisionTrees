d48c9f1fb6bfbc808475220109733192e4371801
rilopez99316 <ilbroberto99316@gmail.com>
2022-04-04 21:31:51 -0600

    Finding the best Decision Tree

--- DecisionTrees.py
+++ DecisionTrees.py
@@ -63,3 +63,29 @@ class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
 fig, ax = plt.subplots(figsize=(8,8))
 tree.plot_tree(model, fontsize=10, ax=ax,class_names=class_names)
 fig.suptitle("Decision tree", fontsize=14)
+
+#3: Find the best performing decision tree (use accuracy - evaluated on the test dataset) 
+#of depth of 20 or less, display it, and print the confusion matrix and accuracy 
+#on the test dataset. Use entropy.
+
+best_model = DecisionTreeClassifier(criterion='entropy')
+best_accuracy = 0
+
+max_depths = list(np.arange(15,21))+[None]
+for max_depth in max_depths:
+  model = DecisionTreeClassifier(criterion='entropy', max_depth = max_depth)
+  model = model.fit(x_train, y_train)
+  pred = model.predict(x_test)
+  accuracy = accuracy_score(y_test, pred)
+
+  if (best_accuracy < accuracy):
+    best_model = model
+    best_accuracy = accuracy
+    best_max_depth = max_depth
+
+  print('Max_depth = {};, accuracy = {:7.4f}'.format(max_depth,accuracy))
+
+print(f'Accuracy {best_accuracy:.4}')
+print("best_max_depth : ", best_max_depth)
+cm = confusion_matrix(y_test, pred)
+print(cm)
