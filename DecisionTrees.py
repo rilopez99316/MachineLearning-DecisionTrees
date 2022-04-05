e3206f9eea2ea8020edcc56e6d60cf6201591d31
rilopez99316 <ilbroberto99316@gmail.com>
2022-04-04 21:27:36 -0600

    Normalized data to label data in binary

--- DecisionTrees.py
+++ DecisionTrees.py
@@ -6,6 +6,8 @@ from sklearn.tree import DecisionTreeClassifier
 import matplotlib.pyplot as plt
 from sklearn import tree
 from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 
 df = pd.read_csv("doh_dataset.zip", compression='gzip')
 
@@ -29,3 +31,30 @@ class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
 fig, ax = plt.subplots(figsize=(8,8))
 tree.plot_tree(model, fontsize=10, ax=ax,class_names=class_names)
 fig.suptitle("Decision tree", fontsize=14)
+
+# Drop rows that have a NaN in them
+df = df.dropna()
+
+# Remove features we don't want
+# In this case, we remove non-float data as that will mess up the student's code
+df.pop('TimeStamp')
+df.pop('SourceIP')
+df.pop('DestinationIP')
+
+# Make a copy here just for visualization purposes
+original_df = df.copy()
+
+# Extract the labels from the dataframe and encode them to integers
+df_labels = df.pop('Label')
+label_encoder = LabelEncoder()
+df_labels = label_encoder.fit_transform(df_labels)
+
+# Prepare arrays to split into training and testing sets
+x_features = df.values
+y_labels = np.array(df_labels).T
+
+# Split into training (70%) and testing (30%)
+x_train, x_test, y_train, y_test = train_test_split(x_features, y_labels, train_size=0.7, random_state=1738, shuffle=True)
+
+print(f"Training Set={x_train.shape}, Testing Set={x_test.shape}")
+
